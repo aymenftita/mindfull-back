679a1a86ea1851cc0f306b5ba84de07b2d1300af
aymenftita <aymen.ftita@esprit.tn>
2025-02-28 19:05:59 +0100

    first push

---
+++ src/main/java/tn/esprit/mindfull/MindfullSprintApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.mindfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfullSprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindfullSprintApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/mindfull/MindfullSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.mindfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindfullSprintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
